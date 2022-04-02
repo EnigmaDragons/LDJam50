39a01ea94e17c3daec36cf036f19e7dba20dfae8
ChaosTheLegend <ivan20131321@gmail.com>
2022-04-02 10:57:26 +0600

    Added a scriptable object for watering tools

---
+++ src/EnigmaDragonsProject/Assets/WateringTool.cs
@@ -0,0 +1,35 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Watering tool")]
+public class WateringTool : ScriptableObject, IWaterHolder
+{
+    public string name;
+    
+    [PreviewField]
+    public Sprite sprite;
+
+    [TextArea]
+    public string description;
+
+    
+    public float range;
+    public float maxCapacity;
+    
+    [SerializeField] private bool upgradable;
+    [ShowIf("upgradable")]
+    [SerializeField] private WateringTool upgradesInto;
+    
+    private float currentWater;
+
+    public bool TryUpgrade(out WateringTool upgrade)
+    {
+        upgrade = null;
+        if (!upgradable) return false;
+        
+        upgrade = upgradesInto;
+        return true;
+    }
+    
+    public float WaterAmount { get => currentWater; set => currentWater = value; }
+}
