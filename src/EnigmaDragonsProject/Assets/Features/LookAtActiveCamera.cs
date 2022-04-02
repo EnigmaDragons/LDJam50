5cd9a89405b3ba122b6a7981ca29768ba3deb26a
Noah Reinagel <noah.r9@gmail.com>
2022-04-02 14:42:09 -0700

    the bars face the camera

---
+++ src/EnigmaDragonsProject/Assets/Features/LookAtActiveCamera.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class LookAtActiveCamera : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+    }
+}
