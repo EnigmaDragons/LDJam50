de48e2ae381efaabcf5877d6904abb8c056a6812
Noah Reinagel <noah.r9@gmail.com>
2022-04-01 21:39:09 -0700

    fixed frame bugs

--- src/EnigmaDragonsProject/Assets/Features/Plant.cs
+++ src/EnigmaDragonsProject/Assets/Features/Plant.cs
@@ -5,7 +5,9 @@ public class Plant : ScriptableObject
 {
     [SerializeField] private float waterCapacity;
     [SerializeField] private float waterConsumptionPerSecond;
+    [SerializeField] private float wiltingSeconds;
 
     public float WaterCapacity => waterCapacity;
     public float WaterConsumption => waterConsumptionPerSecond;
+    public float WiltingSeconds => wiltingSeconds;
 }
--- src/EnigmaDragonsProject/Assets/Features/PlantController.cs
+++ src/EnigmaDragonsProject/Assets/Features/PlantController.cs
@@ -1,21 +1,35 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlantController : MonoBehaviour
 {
     [SerializeField] private Plant plant;
-    [SerializeField] private float currentWater;
-    [SerializeField] private float healthInSeconds;
     [SerializeField] private Navigator navigator;
 
-    private void Awake() => currentWater = plant.WaterCapacity;
+    private float _currentWater;
+    private float _wiltingSeconds;
+    
+    private void Awake()
+    {
+        _currentWater = plant.WaterCapacity;
+        _wiltingSeconds = plant.WiltingSeconds;
+    }
     
     private void Update()
     {
-        if(currentWater > 0)
-            currentWater -= Time.deltaTime * plant.WaterConsumption;
-        else if(healthInSeconds > 0)
-            healthInSeconds -= Time.deltaTime;
-        else
+        if (_currentWater > 0)
+        {
+            _currentWater -= Time.deltaTime * plant.WaterConsumption;
+            if (_currentWater < 0)
+            {
+                _wiltingSeconds += _currentWater / plant.WaterConsumption;
+                _currentWater = 0;
+            }
+        }
+        else 
+            _wiltingSeconds -= Time.deltaTime;
+
+        if (_wiltingSeconds <= 0)
             navigator.NavigateToGameOverScene();
     }
 }
