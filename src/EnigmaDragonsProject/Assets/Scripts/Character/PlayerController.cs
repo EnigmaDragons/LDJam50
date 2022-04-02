31b791af108441023c129c3ff1848deeb4a647fb
SilasReinagel <silas.reinagel@gmail.com>
2022-04-01 23:47:49 -0700

    Coding: Plugged in basic Walk/Idle flow for player character. Closes #60

--- src/EnigmaDragonsProject/Assets/Scripts/Character/PlayerController.cs
+++ src/EnigmaDragonsProject/Assets/Scripts/Character/PlayerController.cs
@@ -5,35 +5,29 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public ExampleCharacterController Character;
-    [SerializeField] private GameObject character;
+    [SerializeField] private Animator animator;
     
-    [SerializeField] private DefaultInputActions.PlayerActions _playerActions;
-    private Quaternion _lookDirection;
+    private bool _isMoving;
 
-    public void FixedUpdate()
-    {
-        Character.UpdateRotation(ref _lookDirection, Time.deltaTime);
-    }
-    
     public void Move(InputAction.CallbackContext context)
     {
         var characterInputs = new PlayerCharacterInputs();
-
         var move = context.action.ReadValue<Vector2>();
         characterInputs.MoveAxisForward = move.y;
         characterInputs.MoveAxisRight = move.x;
-        
         Character.SetInputs(ref characterInputs);
+        
+        var isMoving = move.magnitude > 0;
+        UpdateIsMoving(isMoving);
         Debug.Log($"Move {move}");
-        //
-        // var movementDirection = new Vector3(move.x, 0, move.y);
-        // movementDirection.Normalize();
-        //
-        // var rotation = Quaternion.LookRotation(character.transform.position + movementDirection, Vector3.up);
-        // if (rotation.x == 0 && rotation.y == 0 && rotation.z == 0 && rotation.w == 0)
-        //     return;
-        // else
-        //     _lookDirection = rotation;
-        // Debug.Log(rotation);
+    }
+
+    private void UpdateIsMoving(bool isMoving)
+    {
+        if (_isMoving == isMoving)
+            return;
+
+        _isMoving = isMoving;
+        animator.SetBool("IsWalking", isMoving);
     }
 }
